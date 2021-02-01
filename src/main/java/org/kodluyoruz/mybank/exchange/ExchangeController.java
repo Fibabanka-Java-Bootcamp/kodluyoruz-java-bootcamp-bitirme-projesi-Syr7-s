747264128aca2d9df1327fde0f2e3913d39d29ac
Syr7-s <isasayar7@github.com>
2021-02-01 22:49:03 +0300

    ExchangeController and ExchangeDto was created.MybankApplication was edited.

---
+++ src/main/java/org/kodluyoruz/mybank/exchange/ExchangeController.java
@@ -0,0 +1,21 @@
+package org.kodluyoruz.mybank.exchange;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/transaction")
+public class ExchangeController {
+    @RequestMapping(value = "/convert")
+    public ExchangeDto getConvert() throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "https://api.exchangeratesapi.io";
+        ExchangeDto exchangeDto = restTemplate.getForObject(uri+"/latest?base=TRY", ExchangeDto.class);
+        /*System.out.println(exchangeDto.getRates().get("USD"));
+        System.out.println(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(exchangeDto));*/
+        return exchangeDto;
+    }
+}
---
+++ src/main/java/org/kodluyoruz/mybank/exchange/ExchangeDto.java
@@ -0,0 +1,14 @@
+package org.kodluyoruz.mybank.exchange;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import java.util.Map;
+@Data
+public class ExchangeDto implements Serializable {
+    private Map<String,Double> rates;
+    private String base;
+    private LocalDate date;
+}
