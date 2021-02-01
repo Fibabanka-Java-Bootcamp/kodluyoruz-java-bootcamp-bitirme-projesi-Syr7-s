2e14499e76bf45ac9b2d4e342fc0d7dcc8fb3998
Syr7-s <isasayar7@github.com>
2021-02-01 22:53:19 +0300

    Serializable was removed in the ExchangeDto.java

--- src/main/java/org/kodluyoruz/mybank/exchange/ExchangeDto.java
+++ src/main/java/org/kodluyoruz/mybank/exchange/ExchangeDto.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 import java.util.Map;
 @Data
-public class ExchangeDto implements Serializable {
+public class ExchangeDto {
     private Map<String,Double> rates;
     private String base;
     private LocalDate date;
