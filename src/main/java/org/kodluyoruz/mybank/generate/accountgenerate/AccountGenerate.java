6b2879a7da6babe2f2cebf10af9e75db223be58a
Syr7-s <isasayar7@github.com>
2021-02-02 21:33:59 +0300

    AccountGenerate was edited.

--- src/main/java/org/kodluyoruz/mybank/generate/accountgenerate/AccountGenerate.java
+++ src/main/java/org/kodluyoruz/mybank/generate/accountgenerate/AccountGenerate.java
@@ -7,6 +7,7 @@ public class AccountGenerate {
     public static Supplier<String> accountGenerate = () -> {
         Random rand = new Random();
         StringBuilder accountNumber = new StringBuilder();
+        //accountNumber.append("85"); i<14
         for (int i=0;i<16;i++) {
             Integer value = rand.nextInt(9);
             accountNumber.append(value);
