8c96a77772c9e1ba65c493483620b02ed1eb92f4
Syr7-s <isasayar7@github.com>
2021-02-03 19:43:02 +0300

    create method was added to the ExtractOfAccountService.

--- src/main/java/org/kodluyoruz/mybank/extractofaccount/service/ExtractOfAccountService.java
+++ src/main/java/org/kodluyoruz/mybank/extractofaccount/service/ExtractOfAccountService.java
@@ -1,7 +1,18 @@
 package org.kodluyoruz.mybank.extractofaccount.service;
 
+import org.kodluyoruz.mybank.extractofaccount.entity.ExtractOfAccount;
+import org.kodluyoruz.mybank.extractofaccount.repository.ExtractOfAccountRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExtractOfAccountService {
+    private final ExtractOfAccountRepository extractOfAccountRepository;
+
+    public ExtractOfAccountService(ExtractOfAccountRepository extractOfAccountRepository) {
+        this.extractOfAccountRepository = extractOfAccountRepository;
+    }
+
+    public ExtractOfAccount create(ExtractOfAccount extractOfAccount){
+        return extractOfAccountRepository.save(extractOfAccount);
+    }
 }
