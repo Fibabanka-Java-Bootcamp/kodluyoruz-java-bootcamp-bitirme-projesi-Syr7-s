ff7db2492305a52f5c0edbe5e18129aa105f3f9c
Syr7-s <isasayar7@github.com>
2021-02-03 21:16:05 +0300

    CreditController was edited.(creditCardDebtInquiry method was added.)

--- src/main/java/org/kodluyoruz/mybank/card/creditcard/controller/CreditCardController.java
+++ src/main/java/org/kodluyoruz/mybank/card/creditcard/controller/CreditCardController.java
@@ -1,17 +1,22 @@
 package org.kodluyoruz.mybank.card.creditcard.controller;
 
 
+import org.kodluyoruz.mybank.card.bankcard.entity.BankCard;
+import org.kodluyoruz.mybank.card.bankcard.service.BankCardService;
 import org.kodluyoruz.mybank.card.creditcard.dto.CreditCardDto;
 import org.kodluyoruz.mybank.card.creditcard.entity.CreditCard;
 import org.kodluyoruz.mybank.card.creditcard.exception.CreditCardNotCreatedException;
 import org.kodluyoruz.mybank.card.creditcard.service.CreditCardService;
 import org.kodluyoruz.mybank.customer.dto.CustomerDto;
 import org.kodluyoruz.mybank.customer.service.CustomerService;
+import org.kodluyoruz.mybank.extractofaccount.entity.ExtractOfAccount;
+import org.kodluyoruz.mybank.extractofaccount.service.ExtractOfAccountService;
 import org.kodluyoruz.mybank.generate.accountgenerate.AccountGenerate;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.constraints.Min;
 import java.util.List;
@@ -22,10 +27,14 @@ import java.util.stream.Collectors;
 public class CreditCardController {
     private final CreditCardService creditCardService;
     private final CustomerService customerService;
+    private final BankCardService bankCardService;
+    private final ExtractOfAccountService extractOfAccountService;
 
-    public CreditCardController(CreditCardService creditCardService, CustomerService customerService) {
+    public CreditCardController(CreditCardService creditCardService, CustomerService customerService, BankCardService bankCardService, ExtractOfAccountService extractOfAccountService) {
         this.creditCardService = creditCardService;
         this.customerService = customerService;
+        this.bankCardService = bankCardService;
+        this.extractOfAccountService = extractOfAccountService;
     }
 
     @PostMapping("/{customerID}/creditCard")
@@ -60,14 +69,37 @@ public class CreditCardController {
     @ResponseStatus(HttpStatus.OK)
     public CreditCardDto doShopping(@PathVariable("creditCardNo") long creditCardNo,
                                     @RequestParam("productPrice") int price) {
-        return creditCardService.update(creditCardNo,price).toCreditCardDto();
+        return creditCardService.update(creditCardNo, price).toCreditCardDto();
     }
+
     @GetMapping("/{creditCardNo}/debt")
-    public String creditCardDebtInquiry(@PathVariable("creditCardNo") long creditCardNo){
+    public String creditCardDebtInquiry(@PathVariable("creditCardNo") long creditCardNo) {
         CreditCard creditCard = creditCardService.getCreditCard(creditCardNo);
         double debt = creditCard.getExtractOfAccount().getTermDebt();
         double minimumPaymentAmount = creditCard.getExtractOfAccount().getMinimumPaymentAmount();
         String nameSurname = creditCard.getCardNameSurname();
-        return nameSurname +" named customer debt : "+ debt +" and minimum payment amount "+ minimumPaymentAmount;
+        return nameSurname + " named customer debt : " + debt + " and minimum payment amount " + minimumPaymentAmount;
+    }
+
+    @PutMapping("/{bankCardNO}/debt/{creditCardNO}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreditCardDto payCreditCardDebt(@PathVariable("bankCardNO") long bankCardNO,
+                                           @PathVariable("creditCardNO") long creditCardNO,
+                                           @Min(value = 4) @RequestParam("password") int password,
+                                           @RequestParam("payMoney") int payMoney, @RequestParam("minimumPayment") double minimumPayment) {
+        BankCard bankCard = bankCardService.findBankCard(bankCardNO);
+        CreditCard creditCard = creditCardService.getCreditCard(creditCardNO);
+        ExtractOfAccount extractOfAccount = creditCard.getExtractOfAccount();
+        if (bankCard.getBankCardPassword() == password) {
+            creditCard.setCardDebt(creditCard.getCardDebt() - payMoney);
+            extractOfAccount.setTermDebt(Math.abs(extractOfAccount.getTermDebt() - payMoney));
+            extractOfAccount.setOldDebt(extractOfAccount.getTermDebt() + (extractOfAccount.getTermDebt() * extractOfAccount.getBankRate()));
+            extractOfAccount.setMinimumPaymentAmount(Math.abs(extractOfAccount.getMinimumPaymentAmount() - minimumPayment));
+            extractOfAccountService.update(extractOfAccount);
+            return creditCardService.updateCard(creditCard).toCreditCardDto();
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"BankCard info is not correct.");
+        }
+
     }
 }
