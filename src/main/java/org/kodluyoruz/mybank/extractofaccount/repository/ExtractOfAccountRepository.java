696ffee1e33e62f2a7fcd4ca10d8b847c57a2e7e
Syr7-s <isasayar7@github.com>
2021-02-03 19:40:32 +0300

    ExtractOfAccountRepository interface was created

---
+++ src/main/java/org/kodluyoruz/mybank/extractofaccount/repository/ExtractOfAccountRepository.java
@@ -0,0 +1,7 @@
+package org.kodluyoruz.mybank.extractofaccount.repository;
+
+import org.kodluyoruz.mybank.extractofaccount.entity.ExtractOfAccount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExtractOfAccountRepository extends CrudRepository<ExtractOfAccount,Integer> {
+}
