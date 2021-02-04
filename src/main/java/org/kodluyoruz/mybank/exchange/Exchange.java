77b115599d4abf6580013bd7f6c0dba3b38a6bd8
Syr7-s <isasayar7@github.com>
2021-02-04 20:50:50 +0300

    Exchange was edited.

--- src/main/java/org/kodluyoruz/mybank/exchange/Exchange.java
+++ src/main/java/org/kodluyoruz/mybank/exchange/Exchange.java
@@ -1,13 +1,13 @@
 package org.kodluyoruz.mybank.exchange;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.function.Function;
 
-@Component
 public class Exchange {
-    public static Function<String, ExchangeDto> getConvert = (base ->  new RestTemplate()
-               .getForObject("https://api.exchangeratesapi.io/latest?base="+base,ExchangeDto.class)
+    private Exchange() {
+    }
+
+    public static final Function<String, ExchangeDto> getConvert = (base -> new RestTemplate()
+            .getForObject("https://api.exchangeratesapi.io/latest?base=" + base, ExchangeDto.class)
     );
 }
