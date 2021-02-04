54c797e5f9de79802836105e5f0d56288a1f0272
Syr7-s <isasayar7@github.com>
2021-02-04 20:36:41 +0300

    BankCardNotDeletedException was created.

---
+++ src/main/java/org/kodluyoruz/mybank/card/bankcard/exception/BankCardNotDeletedException.java
@@ -0,0 +1,11 @@
+package org.kodluyoruz.mybank.card.bankcard.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BankCardNotDeletedException extends RuntimeException{
+    public BankCardNotDeletedException(String message) {
+        super(message);
+    }
+}
