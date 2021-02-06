7ba1b23a9886aacdfdb184c543d007e63cacdf01
Syr7-s <isasayar7@github.com>
2021-02-06 15:38:34 +0300

    ExtractOfController and ExtractOfAccountService were edited.

--- src/main/java/org/kodluyoruz/mybank/extractofaccount/controller/ExtractOfController.java
+++ src/main/java/org/kodluyoruz/mybank/extractofaccount/controller/ExtractOfController.java
@@ -1,10 +1,8 @@
 package org.kodluyoruz.mybank.extractofaccount.controller;
 
-import lombok.Getter;
 import org.kodluyoruz.mybank.card.creditcard.entity.CreditCard;
 import org.kodluyoruz.mybank.card.creditcard.service.CreditCardService;
 import org.kodluyoruz.mybank.extractofaccount.dto.ExtractOfAccountDto;
-import org.kodluyoruz.mybank.extractofaccount.entity.ExtractOfAccount;
 import org.kodluyoruz.mybank.extractofaccount.service.ExtractOfAccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +14,14 @@ public class ExtractOfController {
     private final ExtractOfAccountService extractOfAccountService;
     private final CreditCardService creditCardService;
 
-    public ExtractOfController(ExtractOfAccountService extractOfAccountService, CreditCardService creditCardService) {
+    public ExtractOfController(ExtractOfAccountService extractOfAccountService,CreditCardService creditCardService) {
         this.extractOfAccountService = extractOfAccountService;
         this.creditCardService = creditCardService;
     }
 
     @PostMapping("/{creditCardNo}")
     @ResponseStatus(HttpStatus.CREATED)
-    public ExtractOfAccount create(@PathVariable("creditCardNo") long creditCardNo, @RequestParam("password") int password,
+    public ExtractOfAccountDto create(@PathVariable("creditCardNo") long creditCardNo, @RequestParam("password") int password,
                                    @RequestBody ExtractOfAccountDto extractOfAccountDto) {
         CreditCard creditCard = creditCardService.getCreditCard(creditCardNo);
         if (creditCard.getCardPassword() == password) {
@@ -31,10 +29,11 @@ public class ExtractOfController {
             extractOfAccountDto.setOldDebt(extractOfAccountDto.getMinimumPaymentAmount());
             extractOfAccountDto.setMinimumPaymentAmount(creditCard.getCardDebt() * 0.3);
             extractOfAccountDto.setCreditCard(creditCard);
-            return extractOfAccountService.create(extractOfAccountDto.toExtractOfAccount());
+            return extractOfAccountService.create(extractOfAccountDto.toExtractOfAccount()).toExtractOfAccountDto();
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "CreditCard info is not correct.");
         }
+
     }
 
     @GetMapping("/{extractNo}/extract")
--- src/main/java/org/kodluyoruz/mybank/extractofaccount/service/ExtractOfAccountService.java
+++ src/main/java/org/kodluyoruz/mybank/extractofaccount/service/ExtractOfAccountService.java
@@ -1,5 +1,6 @@
 package org.kodluyoruz.mybank.extractofaccount.service;
 
+
 import org.kodluyoruz.mybank.extractofaccount.entity.ExtractOfAccount;
 import org.kodluyoruz.mybank.extractofaccount.repository.ExtractOfAccountRepository;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class ExtractOfAccountService {
     public ExtractOfAccount create(ExtractOfAccount extractOfAccount) {
         return extractOfAccountRepository.save(extractOfAccount);
     }
+
 
     public Optional<ExtractOfAccount> get(int extractNo) {
         return extractOfAccountRepository.findById(extractNo);
