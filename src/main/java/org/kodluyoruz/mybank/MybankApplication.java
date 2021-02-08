78c5de40da50932c235c622d08967bbb988620e0
Syr7-s <isasayar7@github.com>
2021-02-08 15:01:00 +0300

    MybankApplication and pom.xml was edited.

--- src/main/java/org/kodluyoruz/mybank/MybankApplication.java
+++ src/main/java/org/kodluyoruz/mybank/MybankApplication.java
@@ -2,8 +2,7 @@ package org.kodluyoruz.mybank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class MybankApplication {
