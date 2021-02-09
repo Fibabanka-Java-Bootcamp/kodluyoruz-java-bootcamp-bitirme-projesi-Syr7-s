39d92172b7acc7183a0029848760326b4fcdab09
Syr7-s <isasayar7@github.com>
2021-02-09 14:00:55 +0300

    BankCardRepository was edited.Also BankCardRepositoryTest and BankCardServiceImplTest was created.

--- src/main/java/org/kodluyoruz/mybank/card/bankcard/abstrct/BankCardRepository.java
+++ src/main/java/org/kodluyoruz/mybank/card/bankcard/abstrct/BankCardRepository.java
@@ -4,7 +4,9 @@ import org.kodluyoruz.mybank.card.bankcard.concrete.BankCard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BankCardRepository extends CrudRepository<BankCard,Long> {
     BankCard findBankCardByBankCardAccountNumber(long bankCardNo);
     Page<BankCard> findAll(Pageable pageable);
---
+++ src/test/java/org/kodluyoruz/mybank/card/bankcard/abstrct/BankCardRepositoryTest.java
@@ -0,0 +1,20 @@
+package org.kodluyoruz.mybank.card.bankcard.abstrct;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.kodluyoruz.mybank.card.bankcard.concrete.BankCard;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardRepositoryTest {
+    @Autowired
+    BankCardRepository bankCardRepository;
+
+    @Test
+    void findBankCardByBankCardAccountNumber(){
+        BankCard bankCard = bankCardRepository.findBankCardByBankCardAccountNumber(8500472435626214L);
+        Assertions.assertEquals("Isa SAYAR",bankCard.getBankCardNameSurname());
+    }
+
+}
