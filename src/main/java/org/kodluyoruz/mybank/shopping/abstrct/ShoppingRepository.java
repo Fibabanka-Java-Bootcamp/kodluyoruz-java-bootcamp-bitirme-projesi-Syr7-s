b2371bb9afc4db126ea88fb68a7c0cc1ecca34ba
Syr7-s <isasayar7@github.com>
2021-02-10 15:53:18 +0300

    @Repository annotation was added to the ShoppingRepository.

--- src/main/java/org/kodluyoruz/mybank/shopping/abstrct/ShoppingRepository.java
+++ src/main/java/org/kodluyoruz/mybank/shopping/abstrct/ShoppingRepository.java
@@ -2,6 +2,8 @@ package org.kodluyoruz.mybank.shopping.abstrct;
 
 import org.kodluyoruz.mybank.shopping.concrete.Shopping;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShoppingRepository extends CrudRepository<Shopping,Integer> {
 }
