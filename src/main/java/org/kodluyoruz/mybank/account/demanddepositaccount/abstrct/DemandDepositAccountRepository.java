a7528b05cdb0d6949eff8ad78232b12431a8b133
Syr7-s <isasayar7@github.com>
2021-02-11 19:36:38 +0300

    findAll method was added to DemandDepositAccountRepository.

--- src/main/java/org/kodluyoruz/mybank/account/demanddepositaccount/abstrct/DemandDepositAccountRepository.java
+++ src/main/java/org/kodluyoruz/mybank/account/demanddepositaccount/abstrct/DemandDepositAccountRepository.java
@@ -1,10 +1,13 @@
 package org.kodluyoruz.mybank.account.demanddepositaccount.abstrct;
 
+import javafx.scene.control.Pagination;
 import org.kodluyoruz.mybank.account.demanddepositaccount.concrete.DemandDepositAccount;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DemandDepositAccountRepository extends CrudRepository<DemandDepositAccount, Long> {
+    Page<DemandDepositAccount> findAll(Pagination pagination);
     DemandDepositAccount findDemandDepositAccountByDemandDepositAccountIBAN(String accountIBAN);
 }
