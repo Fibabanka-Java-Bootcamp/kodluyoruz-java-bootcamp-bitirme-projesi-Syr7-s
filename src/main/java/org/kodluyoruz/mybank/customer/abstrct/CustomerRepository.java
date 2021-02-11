eb4ce7d42ef46062db610d1e0a309f49dab1f0af
Syr7-s <isasayar7@github.com>
2021-02-11 19:25:09 +0300

    Pagination process, I mean findAll method was added to the CustomerRepository.

--- src/main/java/org/kodluyoruz/mybank/customer/abstrct/CustomerRepository.java
+++ src/main/java/org/kodluyoruz/mybank/customer/abstrct/CustomerRepository.java
@@ -1,11 +1,14 @@
 package org.kodluyoruz.mybank.customer.abstrct;
 
+import org.kodluyoruz.mybank.account.savingsaccount.concrete.SavingsAccount;
 import org.kodluyoruz.mybank.customer.concrete.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer,Long> {
-    //Customer findCustomerByCustomerID(long id);
+    Page<Customer> findAll(Pageable pageable);
     Customer findCustomerByCustomerTC(long id);
 }
