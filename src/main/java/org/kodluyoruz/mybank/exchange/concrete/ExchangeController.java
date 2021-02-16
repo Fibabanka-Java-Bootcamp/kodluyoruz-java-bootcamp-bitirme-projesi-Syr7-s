486ead5b6e871f2effeda204bb226218ebf0a710
Syr7-s <isasayar7@github.com>
2021-02-16 19:20:28 +0300

    ExchangeController was edited.

--- src/main/java/org/kodluyoruz/mybank/exchange/concrete/ExchangeController.java
+++ src/main/java/org/kodluyoruz/mybank/exchange/concrete/ExchangeController.java
@@ -1,16 +1,18 @@
 package org.kodluyoruz.mybank.exchange.concrete;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/api/transaction")
+@RequestMapping("/api/v1/transaction")
 public class ExchangeController {
-    @RequestMapping(value = "/convert/{base}")
+
+    @GetMapping(value = "/convert/{base}")
     public ExchangeDto getConvert(@PathVariable("base") String base) {
         return new RestTemplate()
-                .getForObject("https://api.exchangeratesapi.io/latest?base="+base,ExchangeDto.class);
+                .getForObject("https://api.exchangeratesapi.io/latest?base=" + base, ExchangeDto.class);
     }
 }
